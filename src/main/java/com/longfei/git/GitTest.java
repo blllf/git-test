9fce8859b4113ed6ffd3e51977d18f4f365158d8
longfei <longfei@qq.com>
2024-01-13 21:08:27 +0800

    master commit2

--- src/main/java/com/longfei/git/GitTest.java
+++ src/main/java/com/longfei/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello world2 !");
         System.out.println("hello world3 !");
         System.out.println("hello world -- fix-01 !");
+        System.out.println("master test");
 
     }
 }
